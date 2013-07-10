22d7946df3307a5a1fcbb688339e67051dbe20ad
Mounir S <errouji15@yahoo.fr>
2013-07-10 23:36:18 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,8 @@
-var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var buff = fs.readFileSync('index.html');
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+var text = buf.toString(buff);
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+  console.log(text);
